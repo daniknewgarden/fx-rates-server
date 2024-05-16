2baf58744b7a30e435aa6236463f8d385c1d3157
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-05-16 15:37:16 +0200

    feat(db): setup mongodb
    add writing fxrates to db

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var URL = "https://due-diligence-hub.com/en/tools/cross_rate_matrix"
@@ -33,6 +38,28 @@ func fetchDocument(url string, retries int) (*goquery.Document, error) {
 }
 
 func main() {
+	// Connect to mongoDB
+	credential := options.Credential{
+   		Username: "root",
+   		Password: "mySecureDbPassword1!",
+	}
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(credential)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+    	log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+    	log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	// Fetch the document
     doc, err := fetchDocument(URL, 3)
     if err != nil {
         fmt.Println("Error:", err)
@@ -47,7 +74,7 @@ func main() {
 		rateValue := cleanRateValue(s.Text())
 
 		// In case with "USD-USD" the rate value is "-"
-		if toCurrency != "USD" {
+		if toCurrency == "USD" {
 			usdFXMatrixMap[toCurrency] = 1
 			return
 		}
@@ -61,5 +88,18 @@ func main() {
 		usdFXMatrixMap[toCurrency] = rate
 	})
 
-	fmt.Println(usdFXMatrixMap)
+	// Save parsed data to MongoDB
+	usdFxMatrixCollection := client.Database("test").Collection("fx-matrix")
+
+	filter := bson.M{"baseCurrency": "USD"}
+	update := bson.M{"$set": bson.M{"fxRates": usdFXMatrixMap}}
+
+	updateOptions := options.Update().SetUpsert(true)
+	_, err = usdFxMatrixCollection.UpdateOne(context.TODO(), filter, update, updateOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("USD FX Matrix saved to MongoDB!", usdFXMatrixMap)
 }
