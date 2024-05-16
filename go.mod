28acff531d53ab1960d7583cd0eaac601e6ef989
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-05-16 12:54:49 +0200

    feat: add web scraping of USD fx-rates

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module fx-rates-rest-api
 
 go 1.21.7
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var URL = "https://due-diligence-hub.com/en/tools/cross_rate_matrix"
+
+func cleanRateValue(rateValue string) string {
+    rateValue = strings.ReplaceAll(rateValue, " ", "")
+    rateValue = strings.Replace(rateValue, "\n", "", -1)
+    rateValue = strings.Replace(rateValue, ",", ".", -1)
+    return rateValue
+}
+
+func fetchDocument(url string, retries int) (*goquery.Document, error) {
+    for i := 0; i < retries; i++ {
+        resp, err := http.Get(url)
+        if err != nil {
+            fmt.Println("Error while fetching:", err)
+            time.Sleep(time.Second * 2)
+            continue
+        }
+        return goquery.NewDocumentFromReader(resp.Body)
+    }
+    return nil, fmt.Errorf("failed to fetch document after %d retries", retries)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+    doc, err := fetchDocument(URL, 3)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+	usdFXMatrixMap := make(map[string]float64)
+
+	usdRow := doc.Find("table.table").Find("tbody").Find("tr").Eq(1)
+	usdRow.Find("[data-base-target]").Each(func(i int, s *goquery.Selection) {
+		toCurrency := strings.ReplaceAll(s.AttrOr("data-base-target", ""), "USD-", "")
+		rateValue := cleanRateValue(s.Text())
+
+		// In case with "USD-USD" the rate value is "-"
+		if toCurrency != "USD" {
+			usdFXMatrixMap[toCurrency] = 1
+			return
+		}
+
+		rate, err := strconv.ParseFloat(rateValue, 64)
+    	if err != nil {
+    	    fmt.Println("Error parsing string:", err)
+    	    return
+    	}
+
+		usdFXMatrixMap[toCurrency] = rate
+	})
+
+	fmt.Println(usdFXMatrixMap)
 }
